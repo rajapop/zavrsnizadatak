29c5b57f64cb868382b74f89a65d445ba9740734
Rajko Popovic <rajapop79@yahoo.com>
2019-04-17 21:05:39 +0200

    zadatak 7 tacka 15

--- sidebar.php
+++ sidebar.php
@@ -1,31 +1,36 @@
+<?php
+
+// pripremamo upit
+$sql5 = "SELECT * FROM posts ORDER BY created_at DESC LIMIT 5";
+$statement = $connection->prepare($sql5);
+
+// izvrsavamo upit
+$statement->execute();
+
+// zelimo da se rezultat vrati kao asocijativni niz.
+// ukoliko izostavimo ovu liniju, vratice nam se obican, numerisan niz
+$statement->setFetchMode(PDO::FETCH_ASSOC);
+
+// punimo promenjivu sa rezultatom upita
+$posts1 = $statement->fetchAll();
+
+// koristite var_dump kada god treba da proverite sadrzaj neke promenjive
+    // echo '<pre>';
+    // var_dump($posts1);
+    // echo '</pre>';
+
+?>
+<!-- <div class="col-sm-8 blog-main"> -->
 <aside class="col-sm-3 ml-sm-auto blog-sidebar">
-            <div class="sidebar-module sidebar-module-inset">
-                <h4>About</h4>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-            </div>
-            <div class="sidebar-module">
-                <h4>Archives</h4>
-                <ol class="list-unstyled">
-                    <li><a href="#">March 2014</a></li>
-                    <li><a href="#">February 2014</a></li>
-                    <li><a href="#">January 2014</a></li>
-                    <li><a href="#">December 2013</a></li>
-                    <li><a href="#">November 2013</a></li>
-                    <li><a href="#">October 2013</a></li>
-                    <li><a href="#">September 2013</a></li>
-                    <li><a href="#">August 2013</a></li>
-                    <li><a href="#">July 2013</a></li>
-                    <li><a href="#">June 2013</a></li>
-                    <li><a href="#">May 2013</a></li>
-                    <li><a href="#">April 2013</a></li>
-                </ol>
-            </div>
-            <div class="sidebar-module">
-                <h4>Elsewhere</h4>
-                <ol class="list-unstyled">
-                    <li><a href="#">GitHub</a></li>
-                    <li><a href="#">Twitter</a></li>
-                    <li><a href="#">Facebook</a></li>
-                </ol>
-            </div>
-        </aside><!-- /.blog-sidebar -->
+    <div class="sidebar-module sidebar-module-inset">
+        <h4>Latest posts</h4>
+        <p>
+            <?php
+                foreach ($posts1 as $post) {
+            ?>
+            <a href="single-post.php?post_id=<?php echo($post['id']) ?>"> - <?php echo($post['title']) ?> <br></a>
+            <?php } ?> 
+        </p>
+    </div> 
+        
+</aside><!-- /.blog-sidebar -->
